e39037a05dd80f0eb85bb5217ba8dd2f12992ea1
Dmitry Slesarev <dvslesar@gmail.com>
2026-01-15 16:05:56 -0300

    Check 'git config --global credential.helper store'

---
+++ balakirev/bb1.py
@@ -0,0 +1,26 @@
+# a = {1,2,3,"hello"}
+# print(a)
+# print(type(a))
+
+# b = {1, 'hello',3,2}
+# a = {1,2,3, 'hello', 2,3,'hello'}
+# print(a)
+# print(b==a)
+
+# a = {1, 4.5, (4, True), 'hi'}
+# print(a)
+# b = {{1,2}, 4}
+# b = {{1:1, 2:2}, 4}
+# b = {[1,2]}
+# print(b)
+# c = set()
+# print(c)
+# print(type(c))
+# c = {}
+# print(type(c))
+
+# d = set([1, 2, 3, 4, 0, 1, 2, 3, -1])
+# print(d)
+# d = set('abrakadabra')
+# print(d)
+
